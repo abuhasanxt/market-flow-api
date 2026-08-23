2d59d2031c99425e2c0ddbd10f6b58481e985e30
unknown <abuhasanxt@gmail.com>
2026-08-23 19:05:01 +0600

    add env config file

---
+++ src/config/env.ts
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+interface EnvConfig {
+  PORT: string;
+  NODE_ENV: string;
+  DATABASE_URL: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_ACCESS_EXPIRES_IN: string;
+}
+
+const loadEnvVariables=():EnvConfig=>{
+const requiredEnvVariable=[
+    "PORT",
+    "NODE_ENV",
+    "DATABASE_URL",
+    "JWT_ACCESS_SECRET",    
+];
+requiredEnvVariable.forEach((variable)=>{
+    if(!process.env[variable]){
+        throw new Error(`Environment variable ${variable} is required but not set in .env file`);
+    }
+});
+return {
+    PORT: process.env.PORT as string,
+    NODE_ENV: process.env.NODE_ENV as string,
+    DATABASE_URL: process.env.DATABASE_URL as string,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+    JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN as string
+};
+}
+export const envConfig:EnvConfig=loadEnvVariables();
