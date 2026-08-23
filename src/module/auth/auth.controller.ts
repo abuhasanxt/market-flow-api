2ce7b7194f910919c367b910d915ed6894f4ae11
unknown <abuhasanxt@gmail.com>
2026-08-23 17:27:00 +0600

    use catchAsync and sendResponce

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,24 +1,20 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { Request, Response } from "express";
 import { authService } from "./auth.service";
+import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
+import status from "http-status";
 
-const registerBuyer = async (req: Request, res: Response) => {
-  try {
-    const result = await authService.registerBuyer(req.body);
-    res.status(200).json({
-      success: true,
-      message: "Buyer register successfully",
-      data: result,
-    });
-  } catch (error: any) {
-    res.status(201).json({
-      success: false,
-      message: "Buyer register failed",
-      error: error.message,
-      details: error,
-    });
-  }
-};
+const registerBuyer = catchAsync(async(req:Request,res:Response)=>{
+  const result =await authService.registerBuyer(req.body)
+  sendResponse(res,{
+    success:true,
+    httpStatusCode:status.CREATED,
+    message:"Buyer Registration successfully",
+    data:result
+
+  })
+})
 
 
 export const authController={
