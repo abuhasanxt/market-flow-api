226c233572d62daf564102522a47538432d782b6
unknown <abuhasanxt@gmail.com>
2026-08-24 14:05:41 +0600

    zod validation auth api

--- src/module/auth/auth.route.ts
+++ src/module/auth/auth.route.ts
@@ -1,15 +1,17 @@
 import express from "express"
 import { authController } from "./auth.controller"
 import { checkAuth } from "../../middleware/checkAuth"
+import { validateRequest } from "../../middleware/valideRequest"
+import { emailVerifyZodSchema, userLoginZodSchema, userRegisterZodSchema } from "./auth.validation"
 
 
 
 const router=express.Router()
 
-router.post("/register",authController.registerBuyer)
-router.post("/login",authController.loginUser)
+router.post("/register",validateRequest(userRegisterZodSchema),authController.registerBuyer)
+router.post("/login",validateRequest(userLoginZodSchema),authController.loginUser)
 router.get("/me", checkAuth(),authController.getMe)
-router.post("/email-verify",authController.verifyEmail)
+router.post("/email-verify",validateRequest(emailVerifyZodSchema),authController.verifyEmail)
 router.post("/refresh",authController.getNewToken)
 router.post("/logout",checkAuth(),authController.logoutUser)
 
