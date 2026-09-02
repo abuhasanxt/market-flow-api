f2ecc66bc5817b3773c83126630e2c12a5c41663
unknown <abuhasanxt@gmail.com>
2026-09-02 08:56:22 +0600

    create webhook controller

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,14 @@ import { IndexRoute } from "./routes";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middleware/globalErrorHandler";
 import { notFound } from "./middleware/notFound";
+import { paymentController } from "./module/payment/payment.controller";
 
 const app: Application = express();
 
 
 
 app.post("/webhook", express.raw({type:"application/json"}), 
-async(req: Request, res: Response) => {
-  console.log("Webhook received", req.body)
-  res.status(200).json({"message": "Webhook received"})  
-}
+paymentController.handlerStripeWebhookEvent
   
 )
 
