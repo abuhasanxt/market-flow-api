3c7685a55f5cd6015620b8a17fa552c7f63358d5
unknown <abuhasanxt@gmail.com>
2026-09-02 09:43:00 +0600

    payout worker logic implement

--- src/queue/payout.worker.ts
+++ src/queue/payout.worker.ts
@@ -1,27 +1,85 @@
 import { Worker } from "bullmq";
 import { redisConnection } from "./connection";
+import { prisma } from "../lib/prisma";
+import { PayoutStatus } from "../../generated/prisma/enums";
 
 export const payoutWorker = new Worker(
   "payout-queue",
   async (job) => {
     console.log("Payout job received!");
 
-    console.log("Job ID:", job.id);
-    console.log("Job Name:", job.name);
-    console.log("Job Data:", job.data);
-
     const { subOrderId, vendorId, amount } = job.data;
 
-    console.log(`Processing payout...`);
-    console.log(`SubOrder: ${subOrderId}`);
-    console.log(`Vendor: ${vendorId}`);
-    console.log(`Amount: ${amount}`);
+console.log("Processing payout for SubOrder:", subOrderId, "Vendor:", vendorId, "Amount:", amount);
 
-    // এখানে পরে actual payout logic আসবে
+    await prisma.$transaction(async (tx) => {
+      // 1. Find SubOrder
+      const subOrder = await tx.subOrder.findUnique({
+        where: {
+          id: subOrderId,
+        },
+      });
+
+      if (!subOrder) {
+        throw new Error(
+          `SubOrder ${subOrderId} not found`,
+        );
+      }
+
+      // 2. Vendor check
+      if (subOrder.vendorId !== vendorId) {
+        throw new Error(
+          `Vendor mismatch for SubOrder ${subOrderId}`,
+        );
+      }
+
+      // 3. Amount check
+      if (subOrder.vendorEarning !== amount) {
+        throw new Error(
+          `Payout amount mismatch for SubOrder ${subOrderId}`,
+        );
+      }
+
+      // 4. Atomic payout claim
+      const payoutClaim = await tx.subOrder.updateMany({
+        where: {
+          id: subOrderId,
+          payoutStatus: PayoutStatus.PENDING,
+        },
+        data: {
+          payoutStatus: PayoutStatus.PAID,
+        },
+      });
+
+      // Already processed
+      if (payoutClaim.count === 0) {
+        console.log(
+          `Payout already processed for ${subOrderId}`,
+        );
+
+        return;
+      }
+
+      //  Update vendor balance
+      await tx.vendor.update({
+        where: {
+          id: vendorId,
+        },
+        data: {
+          balance: {
+            increment: amount,
+          },
+        },
+      });
+
+      console.log(
+        `Payout ${amount} added to vendor ${vendorId}`,
+      );
+    });
   },
   {
     connection: redisConnection,
-  }
+  },
 );
 
 payoutWorker.on("completed", (job) => {
@@ -29,5 +87,8 @@ payoutWorker.on("completed", (job) => {
 });
 
 payoutWorker.on("failed", (job, error) => {
-  console.error(`Payout job ${job?.id} failed:`, error);
+  console.error(
+    `Payout job ${job?.id} failed:`,
+    error,
+  );
 });
